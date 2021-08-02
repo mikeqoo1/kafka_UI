c4ea77afda26aea02ee81bfa00c1a5ee28f728e2
mike.lin <110084@concords.com.tw>
2021-08-02 17:00:16 +0800

    更新main.js package.json

--- src/main.js
+++ src/main.js
@@ -5,17 +5,4 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 createApp(App).use(VueAxios, axios)
-// import Vue from 'vue'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// Make BootstrapVue available throughout your project
-// Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-// Vue.use(IconsPlugin)
-
-
 createApp(App).mount('#app')
